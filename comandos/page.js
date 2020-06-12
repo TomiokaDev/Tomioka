26d95379b6b7170e0b3afca04c4c15a480351aea
Glitch (tomiokabot) <none>
2020-06-12 23:21:40 +0000

    🎥🌈 Updated with Glitch

--- comandos/page.js
+++ comandos/page.js
@@ -6,7 +6,7 @@ module.exports = (client, message, args) => {
   if(cooldown.has(message.author.id)) return message.channel.send("Espera 5 segundos")
    let creador = client.users.cache.get("178651638209314816")
     const embed = new Discord.MessageEmbed()
-    .setDescription(`Accede a la página de ${creador.username} [cliqueando aquí](https://www.supahfox.tk) (actualmente en mantenimiento)`)
+    .setDescription(`Accede a la página de ${creador.username} [cliqueando aquí](https://tomiokabot.wixsite.com/tomiokabot) (actualmente activa)`)
     .setColor(config.color)
     .setFooter(`Ejecutado por: ${message.author.tag}`, message.author.avatarURL())
     message.channel.send({ embed: embed })
