06cc7c7350ce99f8fb4d4581ce89b1d3b29035c4
Glitch (tomiokabot) <none>
2020-06-12 22:25:38 +0000

    ⛹📈 Updated with Glitch

--- comandos/reload.js
+++ comandos/reload.js
@@ -1,1 +1,16 @@
 const Discord = require('discord.js');
+module.exports = (client, message, args) => {
+  if(!args || args.length < 1) return message.reply("pon el nombre de un comando");
+  const commandName = args[0];
+  // Check if the command exists and is valid
+  if(!client.commands.has(commandName)) {
+    return message.send("Ese comando no existe");
+  }
+  // the path is relative to the *current folder*, so just ./filename.js
+  delete require.cache[require.resolve(`./${commandName}.js`)];
+  // We also need to delete and reload the command from the client.commands Enmap
+  client.commands.delete(commandName);
+  const props = require(`./${commandName}.js`);
+  client.commands.set(commandName, props);
+  return message.channel.send(`The command ${commandName} has been reloaded`);
+}
--- comandos/report.js
+++ comandos/report.js
@@ -2,18 +2,31 @@ const Discord = require('discord.js');
 const config = require('../config.js'); 
 const cooldown = new Set();
 
-module.exports = (client, message, args) => {    
-  if(cooldown.has(message.author.id)) return message.channel.send("Espera 5 segundos")
-const embed = new Discord.MessageEmbed()
-  .setTitle ("Para reportar o sugerir, hace click acá")
-  .setURL ('https://forms.gle/Monzo7vQE9d7GNjRA')
+module.exports = async (client, message, args) => {
+
+  if(cooldown.has(message.author.id)) return message.channel.send("Espera 5 segundos")                                                                         
+    let creador = client.users.cache.get("178651638209314816")
+    let sugerencia = message.content.split(' ').slice(1).join(' ')
+    if(!sugerencia) return message.reply("falta un contenido.")
+  const embed = new Discord.MessageEmbed() 
+ .setTitle(`Sugerencia global`)
+  .setDescription(`**Aporte:** ${sugerencia}\n**Sugerente:** ${message.author.tag}`)
+ .setColor(config.color)
+   .setFooter(`Servidor: ${message.guild}`, message.guild.iconURL)
+ .setThumbnail(message.author.avatarURL())
+  creador.send({ embed: embed })
+  
+  const res = new Discord.MessageEmbed()
+  .setTitle(`Tu reporte ha sido enviado!`)
   .setColor(config.color)
   .setFooter(`Servidor: ${message.guild}`, message.guild.iconURL())
-  message.channel.send({ embed: embed })
+ .setThumbnail(message.author.avatarURL({ dynamic:false , format: 'png', size: 1024 }))
+  return message.channel.send({ embed: res })
+  
+message.delete(1000)
   
   cooldown.add(message.author.id); //agregas al autor en el cooldown
   setTimeout(() => {
     cooldown.delete(message.author.id); //elimina el cooldown segun el tiempo que pongas
   }, 5000) //1 seg = 1000ms
- 
 }
