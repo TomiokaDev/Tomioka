83f6ff111042e18d5aa0d2da59aa3b8c3f95b6e7
Glitch (tomiokabot) <none>
2020-06-14 00:23:39 +0000

    👱🐋 Updated with Glitch

--- comandos/suck.js
+++ comandos/suck.js
@@ -4,20 +4,21 @@ const fetch = require("node-fetch");
 const nsfwchannel = new db.crearDB("canales_nsfw");
 const client = new Discord.Client();
 module.exports = async (bot, message, args) => {
-  var member =
-      
-    message.mentions.users.first() || message.guild.members.cache.get(args.join(" "));
+  var member = message.mentions.users.first() || message.guild.members.cache.get(args.join(" "));
   
   try {
-    let guild = client.guild.id.cache.get 
-    let channel = message.guild.id()
+    let guild = message.guild
     if (member === message.author) return message.channel.send("¿Como te nalguearias a ti mismo?"); // estoo
     if (member === bot.user) return message.channel.send("owo atrevido!"); // estoo
     
-    let canalnsfw = await nsfwchannel.get(`${channel.guild.id}`);
-if(!message.channel.nsfw) return message.canal.send('Necesitas estar en un canal Nsfw para hacer eso, por favor ve a' + nsfwchannel)
+  let canalnsfw = await nsfwchannel.get(`${message.guild.id}`)
+    console.log(canalnsfw)
+    //Operador ternario, nsfwChannel tiene la id del server ? si la tiene la variable es dicha id : no la tiene el valor de la variable es Null
+if(!message.channel.nsfw) return message.channel.send('Necesitas estar en un canal Nsfw para hacer eso, por favor ve a <#' + canalnsfw + ">")
     
     let canal = client.channels.cache.get(canalnsfw);
+    
+
     
     const suckFetch = await fetch("https://nekos.life/api/v2/img/bj"),
       suckImg = await suckFetch.json();
