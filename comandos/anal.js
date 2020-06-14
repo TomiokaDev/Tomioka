96bb725bb9be4f1e552c2e0d10a87f7012d0dc53
Glitch (tomiokabot) <none>
2020-06-14 06:18:12 +0000

    👖🎅 Updated with Glitch

--- comandos/anal.js
+++ comandos/anal.js
@@ -25,7 +25,7 @@ if(!message.channel.nsfw) return message.channel.send('Necesitas estar en un can
         
         description: member
           ? `**${message.member.displayName}** le hace un anal a **${member.username}** owo!`
-          : `**${message.member.displayName}** Debes mencionar a alguien, si no tienes con quien ven usa -masturbate`,
+          : `**${message.member.displayName}** Debes mencionar a alguien`,
         image: {
           url: member
             ? analImg.url
