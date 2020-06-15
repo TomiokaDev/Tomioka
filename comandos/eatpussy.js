16b92dad3fec94af4abe6ef45cc6234a3fd9bdb5
Glitch (tomiokabot) <none>
2020-06-15 01:16:19 +0000

    🙎🚀 Updated with Glitch

--- comandos/eatpussy.js
+++ comandos/eatpussy.js
@@ -14,8 +14,8 @@ module.exports = async (bot, message, args) => {
   let canalnsfw = await nsfwchannel.get(`${message.guild.id}`)
     console.log(canalnsfw)
     //Operador ternario, nsfwChannel tiene la id del server ? si la tiene la variable es dicha id : no la tiene el valor de la variable es Null
+    if(!canalnsfw) return message.channel.send('No hay ningun canal definido');
 if(!message.channel.nsfw) return message.channel.send('Necesitas estar en un canal Nsfw para hacer eso, por favor ve a <#' + canalnsfw + ">")
-   if(!nsfwchannel) return message.channel.send('No hay ningun canal definido');
        const eatpussyFetch = await fetch("https://nekos.life/api/v2/img/les"),
       eatpussyImg = await eatpussyFetch.json();
       if(!message.channel.nsfw) return message.channel.send('Necesitas estar en un canal Nsfw para hacer eso <:PoohMamadisimoPorMiel_NRC:640240069252481044>, por favor ve a <#637133413203181570>')    
