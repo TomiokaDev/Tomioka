427f26a7080f06c931cd0b15dd43ef82b2a32840
Glitch (tomiokabot) <none>
2020-06-15 01:08:27 +0000

    ✂️🙉 Updated with Glitch

--- comandos/anal.js
+++ comandos/anal.js
@@ -15,6 +15,7 @@ module.exports = async (bot, message, args) => {
     console.log(canalnsfw)
     //Operador ternario, nsfwChannel tiene la id del server ? si la tiene la variable es dicha id : no la tiene el valor de la variable es Null
 if(!message.channel.nsfw) return message.channel.send('Necesitas estar en un canal Nsfw para hacer eso, por favor ve a <#' + canalnsfw + ">")
+   if(!canalnsfw) return message.channel.send('No hay ningun canal definido');
     
     let canal = client.channels.cache.get(canalnsfw);
     const analFetch = await fetch("https://nekos.life/api/v2/img/anal"),
