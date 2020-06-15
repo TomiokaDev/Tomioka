3e97e8a501ddf4d1ac57870d54bef1de06940490
Glitch (tomiokabot) <none>
2020-06-15 02:17:21 +0000

    🍽💾 Updated with Glitch

--- comandos/restart.js
+++ comandos/restart.js
@@ -1,4 +1,6 @@
 const Discord = require('discord.js');
+const config = require('../config.js');
+const ready = require('../eventos/ready.js') 
 module.exports = async (client, message, args) => {
     if(message.author.id !== '178651638209314816') return message.reply("Acceso denegado.");
      message.channel.send("Reiniciando...").then(async msg => {
@@ -7,6 +9,5 @@ module.exports = async (client, message, args) => {
         await client.login(process.env.TOKEN)
         await msg.edit("Restarting, please wait...")
         msg.edit("Success!")
-      
     })
  }
