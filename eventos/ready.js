248cf65ae0006ab24e1a5b4a9508da51b0a24a7b
Glitch (tomiokabot) <none>
2020-06-16 22:46:47 +0000

    🐥❄️ Updated with Glitch

--- eventos/ready.js
+++ eventos/ready.js
@@ -5,7 +5,7 @@ module.exports = (client, member) => {
  
   setInterval(function() {
 
-    var estados = [`tk!help | Estoy en ${client.guilds.cache.size} servidores.`, `tk!help | Veo ${client.users.cache.size} usuarios.`, `tk!report por algun fallo o sugerencia.`]
+    var estados = [`tk!help | Estoy en ${client.guilds.cache.size} servidores.`, `tk!help | Veo ${client.users.cache.size} usuarios.`, `tk!report por algun fallo o sugerencia.`, `tk!donate para donar al bot. Se agradecería un montón!`]
 
     let estado = estados[Math.floor(estados.length * Math.random())];
 
