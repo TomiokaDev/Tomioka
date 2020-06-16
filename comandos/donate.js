e6d4b11e19832344517c873317512c5b3f84f262
Glitch (tomiokabot) <none>
2020-06-16 00:47:01 +0000

    🕵🏖 Updated with Glitch

--- comandos/donate.js
+++ comandos/donate.js
@@ -8,7 +8,7 @@ module.exports = (client, message, args) => {
  const embed = new Discord.MessageEmbed()
  .setTitle("Donaciones")
  .setDescription("Nos encantaría que nos dones para mantener este bot!")
- .addField("Metodos de pago:", `[PayPal](https://www.paypal.me/supahfoxeh) [Patreon](https://www.patreon.com/supahfox)`)
+ .addField("Metodos de pago:", `[PayPal](https://www.paypal.me/supahfoxeh) [Patreon](https://www.patreon.com/supahfox) [bitcoin](https://blockchain.com/btc/payment_request?address=1LyjBywpJGfyz512kBg8z1VzUgj8w44Qz9&amount=0.00053217&message=Donación Tomioka)`)
  .setColor(config.color)
  .setFooter(`Ejecutado por: ${message.author.tag}`, message.author.avatarURL());
   message.channel.send({ embed: embed })
