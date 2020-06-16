f1e949968ba937334cc0256d13471e6b224405ab
Glitch (tomiokabot) <none>
2020-06-16 19:22:05 +0000

    👍🌿 Updated with Glitch

--- comandos/setnsfw.js
+++ comandos/setnsfw.js
@@ -4,14 +4,14 @@ const Discord = require('discord.js');
 const megadb = require("megadb"); 
 const cooldown = new Set();
 
-module.exports = (client, message, args) => {
-  
+module.exports = async (client, message, args) => {
+  let canalnsfw = await nsfwchannel.get(`${message.guild.id}`)
   let channel = message.mentions.channels.first() || client.channels.cache.get(args[0])
   
+  if(!message.channel.nsfw) return message.channel.send("No mencionaste un canal nsfw, por favor, menciona uno.")
   
 if(!channel){
     return message.reply("Debes proporcionar la ID o mencionar el canal donde iran los comandos NSFW")
-  
   }else{
   
   let embed = new Discord.MessageEmbed()
