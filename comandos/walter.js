d537255e083dc66c7b70d7f2d524b4c0cc270cd0
Glitch (tomiokabot) <none>
2020-06-17 23:06:28 +0000

    🎳👂 Updated with Glitch

---
+++ comandos/walter.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+const config = require('../config.js'); 
+const cooldown = new Set();
+
+module.exports = (client, message, args) => {
+  if(cooldown.has(message.author.id)) return message.channel.send("Espera 5 segundos")
+const embed = new Discord.MessageEmbed()
+    .setTitle("Encontraste al walter de la suerte :)")
+    .setColor(config.color)
+    .setImage("https://cdn.discordapp.com/attachments/671170382010515466/722946412026789918/6rxud0q5zwc31.jpg")
+    .setFooter(`Ejecutado por: ${message.author.tag}`, message.author.avatarURL())
+     message.channel.send({ embed: embed })
+  
+  cooldown.add(message.author.id); //agregas al autor en el cooldown
+  setTimeout(() => {
+    cooldown.delete(message.author.id); //elimina el cooldown segun el tiempo que pongas
+  }, 5000) //1 seg = 1000ms
+}
