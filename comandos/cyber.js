3935a803bf4bc6af231f2478b64fc0f307836ec9
Glitch (tomiokabot) <none>
2020-06-18 21:20:24 +0000

    ⏱🏨 Updated with Glitch

--- comandos/cyber.js
+++ comandos/cyber.js
@@ -6,8 +6,8 @@ module.exports = (client, message, args) => {
   if(cooldown.has(message.author.id)) return message.channel.send("Espera 5 segundos")               //tk!tabien
  let owner = message.guild.owner.user
  const embed = new Discord.MessageEmbed()
- .setDescription(`${message.author.username} dijo que ta mal :thumbsdown:`)
- .setImage("https://cdn.discordapp.com/attachments/671170382010515466/723285222858162196/5e6d197e707fe.jpg")
+ .setDescription(`${message.author.username} dice que te vayas del cyber`)
+ .setImage("https://cdn.discordapp.com/attachments/671170382010515466/723285277115940875/ya-vete-del-ciber01588483563.jpg")
  .setFooter(`Ejecutado por: ${message.author.tag}`, message.author.avatarURL())
  .setColor(config.color)
  message.channel.send({ embed: embed })
--- comandos/punch.js
+++ comandos/punch.js
@@ -8,7 +8,7 @@ module.exports = (client, message, args) => {
     const embednopersona = new Discord.MessageEmbed() 
     .setTitle("Te pegaste a vos mismo xd")
     .setColor(config.color)
-    .setImage("https://scontent.fmvd1-1.fna.fbcdn.net/v/t1.0-9/61291055_1057428687785163_2383982070784327680_n.jpg?_nc_cat=106&_nc_sid=e007fa&_nc_ohc=d2d9V5NkN_oAX8mQzf6&_nc_ht=scontent.fmvd1-1.fna&oh=6d16f5b237fad43d7e719935111f5416&oe=5EE5B1A7")
+    .setImage("https://cdn.discordapp.com/attachments/671170382010515466/723285093237653515/73023181_100608781371310_6278573838301134848_n.jpg")
     .setFooter(`Ejecutado por: ${message.author.tag}`, message.author.avatarURL());
         if(!persona) return message.channel.send({ embed: embednopersona })
     const embedpersona = new Discord.MessageEmbed() 
