f75d7d34fcf03162b54b3eb2f1790827c4c7e33a
SupahFox <31490939+SupahFox@users.noreply.github.com>
2020-06-26 11:26:05 -0300

    COMANDOS V2
    SINCRONIZADOS CON EL SERVIDOR

--- comandos/tabien.js
+++ comandos/tabien.js
@@ -7,7 +7,7 @@ module.exports = (client, message, args) => {
   let owner = message.guild.owner.user
  const embed = new Discord.MessageEmbed()
  .setDescription(`${message.author.username} dijo que ta bien :thumbsup:`)
- .setImage("https://cdn.discordapp.com/attachments/671170382010515466/720322865924145202/download.jpg")
+ .setImage("https://cdn.discordapp.com/attachments/671170382010515466/725042803566051429/download.jpg")
  .setFooter(`Ejecutado por: ${message.author.tag}`, message.author.avatarURL())
  .setColor(config.color)
  message.channel.send({ embed: embed })
