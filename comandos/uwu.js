27f9cc56bdbeeaf257d2aeeac34b87a6ee83e332
SupahFox <31490939+SupahFox@users.noreply.github.com>
2020-07-06 18:20:20 -0300

    Create uwu.js

---
+++ comandos/uwu.js
@@ -0,0 +1,29 @@
+const Discord = require("discord.js");
+const fetch = require("node-fetch");
+const client = new Discord.Client();
+const config = require('../config.js');
+const cooldown = new Set();
+module.exports = async (client, message, args) => {
+  var member = message.mentions.users.first() || message.guild.members.cache.get(args.join(" "));
+
+  try {
+     let uwu = ["https://cdn.discordapp.com/attachments/671170382010515466/729804280487346266/e2360a67f444c73526e88bedd0ea6447.jpg", "https://cdn.discordapp.com/attachments/671170382010515466/729804283343667301/ee47b8a3c0efa59c1cb393e637ed02b9.jpg"]
+    
+     let captura = uwu[Math.floor(uwu.length * Math.random())];
+    
+    const embed = new Discord.MessageEmbed()
+     .setTitle("uwu")
+    .setDescription("uwu")
+    .setImage(captura)
+    .setColor(config.color)
+    .setFooter(`Ejecutado por: ${message.author.tag}`, message.author.avatarURL())
+    return message.channel.send({ embed : embed });
+  } catch (err) {
+    console.log(err);
+    return message.reply("Hubo un error"); // estoo
+  }
+};
+module.exports.config = {
+  command: "uwu",
+  aliases: ["uwu"]
+};
