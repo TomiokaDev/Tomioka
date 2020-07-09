265105465a8a32bff4eb44bc0ebc2d6f5e401155
SupahFox <31490939+SupahFox@users.noreply.github.com>
2020-07-09 16:12:08 -0300

    Update ready.js

--- eventos/ready.js
+++ eventos/ready.js
@@ -5,7 +5,7 @@ module.exports = (client, member) => {
  
   setInterval(function() {
 
-    var estados = [`tk!help | Estoy en ${client.guilds.cache.size} servidores.`, `tk!help | Veo ${client.users.cache.size} usuarios.`, `tk!report por algun fallo o sugerencia.`, `Bot en desarrollo!`]
+    var estados = [`tk!help | Estoy en ${client.guilds.cache.size} servidores.`, `tk!help | Veo ${client.users.cache.size} usuarios.`, `tk!report por algun fallo o sugerencia.`, `Bot en desarrollo!`, `tk!invite | Estoy en ${client.guilds.cache.size} servidores.`, `tk!invite | Veo ${client.users.cache.size} usuarios.`]
 
     let estado = estados[Math.floor(estados.length * Math.random())];
 
