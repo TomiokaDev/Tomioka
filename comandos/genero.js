badea86dad829f68a2580048a85b9f2d4d8adffa
SupahFox <31490939+SupahFox@users.noreply.github.com>
2020-07-09 18:55:51 -0300

    Create genero.js

---
+++ comandos/genero.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+const config = require('../config.js'); 
+const cooldown = new Set();
+
+module.exports = (client, message, args) => { 
+  if(cooldown.has(message.author.id)) return message.channel.send("Espera 5 segundos")
+ message.channel.send("El comando se trasladó a ``tk!orientation``")
+  
+  cooldown.add(message.author.id); //agregas al autor en el cooldown
+  setTimeout(() => {
+    cooldown.delete(message.author.id); //elimina el cooldown segun el tiempo que pongas
+  }, 5000) //1 seg = 1000ms
+ 
+ 
+}
