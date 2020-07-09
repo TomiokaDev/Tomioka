441995372d34afba6d28f516d49a7e5dad93ca62
SupahFox <31490939+SupahFox@users.noreply.github.com>
2020-07-09 16:05:14 -0300

    Create invite.js

---
+++ comandos/invite.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+const config = require('../config.js'); 
+const cooldown = new Set();
+
+module.exports = (client, message, args) => {
+ if(cooldown.has(message.author.id)) return message.channel.send("Espera 5 segundos")
+ message.channel.send("Para invitar el bot entrá acá: https://top.gg/bot/531408067993141248")
+  
+  cooldown.add(message.author.id); //agregas al autor en el cooldown
+  setTimeout(() => {
+    cooldown.delete(message.author.id); //elimina el cooldown segun el tiempo que pongas
+  }, 5000) //1 seg = 1000ms
+ 
+ 
+}
