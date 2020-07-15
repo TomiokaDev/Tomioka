fe3913bc1f631879559ccb8227e781e48696e8cf
SupahFox <31490939+SupahFox@users.noreply.github.com>
2020-07-15 15:14:44 -0300

    COMMAND UPDATE

--- comandos/tamaomeno.js
+++ comandos/tamaomeno.js
@@ -2,12 +2,12 @@ const Discord = require('discord.js');
 const config = require('../config.js'); 
 const cooldown = new Set();
 
-module.exports = (client, message, args) => {
-  if(cooldown.has(message.author.id)) return message.channel.send("Espera 5 segundos")
-  let owner = message.guild.owner.user
+module.exports = (client, message, args) => {     
+  if(cooldown.has(message.author.id)) return message.channel.send("Espera 5 segundos")               //tk!tabien
+ let owner = message.guild.owner.user
  const embed = new Discord.MessageEmbed()
- .setDescription(`**${message.author.username}** se come una galletita uwu`)
- .setImage("https://cdn.discordapp.com/attachments/671170382010515466/716421764401332254/31d1baa26c7c31e22b2e065f7dd4493abeb9ae5a_hq.gif")
+ .setDescription(`${message.author.username} dijo que ta maomeno :thumbsup::thumbsdown:`)
+ .setImage("https://cdn.discordapp.com/attachments/671170382010515466/728673584142090250/hqdefault.png")
  .setFooter(`Ejecutado por: ${message.author.tag}`, message.author.avatarURL())
  .setColor(config.color)
  message.channel.send({ embed: embed })
@@ -16,5 +16,4 @@ module.exports = (client, message, args) => {
   setTimeout(() => {
     cooldown.delete(message.author.id); //elimina el cooldown segun el tiempo que pongas
   }, 5000) //1 seg = 1000ms
- 
 }
--- comandos/trespingos.js
+++ comandos/trespingos.js
@@ -6,7 +6,7 @@ module.exports = (client, message, args) => {
   if(cooldown.has(message.author.id)) return message.channel.send("Espera 5 segundos")               //tk!tabien
  let owner = message.guild.owner.user
  const embed = new Discord.MessageEmbed()
- .setDescription(`A ${message.author.username} le chupo 3 pingos`)
+ .setDescription(`A ${message.author.username} le chupó 3 pingos`)
  .setImage("https://cdn.discordapp.com/attachments/671170382010515466/728671363170041926/EZn-iNYWkAEforF.jpg")
  .setFooter(`Ejecutado por: ${message.author.tag}`, message.author.avatarURL())
  .setColor(config.color)
