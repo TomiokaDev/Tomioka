a70f6a1293b8dc361b65a3aa2f1278e41ccd3aa2
SupahFox <31490939+SupahFox@users.noreply.github.com>
2020-07-22 00:42:11 -0300

    Create say.js

---
+++ comandos/say.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+const config = require('../config.js'); 
+const cooldown = new Set();
+
+module.exports = (client, message, args) => {     
+  if(cooldown.has(message.author.id)) return message.channel.send("Espera 5 segundos")                  //tk!question
+ let say = message.content.split(' ').slice(1).join(' ')
+ message.delete({ timeout: 100 })
+ if(!say) return message.reply("debes preguntarme algo.");
+ message.channel.send(say)
+  
+  cooldown.add(message.author.id); //agregas al autor en el cooldown
+  setTimeout(() => {
+    cooldown.delete(message.author.id); //elimina el cooldown segun el tiempo que pongas
+  }, 5000) //1 seg = 1000ms
+ 
+ 
+}
