61d03a9fc391a75dbedd7c0c6c850d99542c90d9
SupahFox <31490939+SupahFox@users.noreply.github.com>
2020-07-23 17:36:52 -0300

    Update and rename matigato.js to facha.js

--- comandos/facha.js
+++ comandos/facha.js
@@ -3,12 +3,14 @@ const config = require('../config.js');
 const cooldown = new Set();
 
 module.exports = (client, message, args) => {
+let matigato = ["https://cdn.discordapp.com/attachments/671170382010515466/735956679870513242/unknown.png", "https://cdn.discordapp.com/attachments/671170382010515466/735953675851137184/unknown.png", "https://cdn.discordapp.com/attachments/671170382010515466/735957778182570114/unknown.png"]
+let facha = matigato[Math.floor(matigato.length * Math.random())];
 if(message.guild.id === '624411964931571722'){
   if(cooldown.has(message.author.id)) return message.channel.send("Espera 5 segundos")
   let owner = message.guild.owner.user
  const embed = new Discord.MessageEmbed()
- .setDescription(`MATIGATO FACHA`)
- .setImage("https://cdn.discordapp.com/attachments/671170382010515466/735953675851137184/unknown.png")
+ .setDescription(`MATIGATO FACHA :sunglasses::moyai:`)
+ .setImage(facha)
  .setFooter(`Ejecutado por: ${message.author.tag}`, message.author.avatarURL())
  .setColor(config.color)
  message.channel.send({ embed: embed })
