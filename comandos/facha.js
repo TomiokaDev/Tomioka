110c1eee7c9b9aff4ddafccf8c563f2e553a2f19
SupahFox <31490939+SupahFox@users.noreply.github.com>
2020-07-23 20:17:18 -0300

    Update facha.js

--- comandos/facha.js
+++ comandos/facha.js
@@ -3,9 +3,9 @@ const config = require('../config.js');
 const cooldown = new Set();
 
 module.exports = (client, message, args) => {
-let matigato = ["https://cdn.discordapp.com/attachments/671170382010515466/735956679870513242/unknown.png", "https://cdn.discordapp.com/attachments/671170382010515466/735953675851137184/unknown.png", "https://cdn.discordapp.com/attachments/671170382010515466/735957778182570114/unknown.png"]
+let matigato = ["https://cdn.discordapp.com/attachments/671170382010515466/735956679870513242/unknown.png", "https://cdn.discordapp.com/attachments/671170382010515466/735953675851137184/unknown.png", "https://cdn.discordapp.com/attachments/671170382010515466/735957778182570114/unknown.png", "https://cdn.discordapp.com/attachments/671170382010515466/735993925101420664/unknown.png", "https://cdn.discordapp.com/attachments/671170382010515466/735994263002677318/unknown.png"]
 let facha = matigato[Math.floor(matigato.length * Math.random())];
-if(message.guild.id === '624411964931571722'){
+if(["624411964931571722","678756451581427743"].includes(message.guild.id)){
   if(cooldown.has(message.author.id)) return message.channel.send("Espera 5 segundos")
   let owner = message.guild.owner.user
  const embed = new Discord.MessageEmbed()
