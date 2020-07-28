85e537bfada9538e989292dec8c4bba5aca88199
SupahFox <31490939+SupahFox@users.noreply.github.com>
2020-07-28 19:05:31 -0300

    Update walter.js

--- comandos/walter.js
+++ comandos/walter.js
@@ -1,16 +1,30 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
+const fetch = require("node-fetch");
+const client = new Discord.Client();
 const config = require('../config.js');
 
-module.exports = (client, message, args) => {
-const embed = new Discord.MessageEmbed()
-    .setDescription("Encontraste al **walter** de la suerte :)")
+module.exports = async (client, message, args) => {
+  var member = message.mentions.users.first() || message.guild.members.cache.get(args.join(" "));
+
+  try {
+     let uwu = ["https://cdn.discordapp.com/attachments/671170382010515466/729804280487346266/e2360a67f444c73526e88bedd0ea6447.jpg", "https://cdn.discordapp.com/attachments/671170382010515466/729804283343667301/ee47b8a3c0efa59c1cb393e637ed02b9.jpg"]
+    
+     let captura = uwu[Math.floor(uwu.length * Math.random())];
+    
+    const embed = new Discord.MessageEmbed()
+     .setTitle("uwu")
+    .setDescription("uwu")
+    .setImage(captura)
     .setColor(config.color)
-    .setImage("https://cdn.discordapp.com/attachments/671170382010515466/722946412026789918/6rxud0q5zwc31.jpg")
     .setFooter(`Ejecutado por: ${message.author.tag}`, message.author.avatarURL())
-     message.channel.send({ embed: embed })
+    return message.channel.send({ embed : embed });
+  } catch (err) {
+    console.log(err);
+    return message.reply("Hubo un error"); // estoo
+  }
 };
 module.exports.config = {
-command:"walter",
-aliases:["walter"],
-cooldown: 5
-}
+  command: "walter",
+  aliases: ["walter"],
+  cooldown: 5
+};
