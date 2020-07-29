b4eb088ad8db4b31a1faa4a0e512c5f8a9e954ba
SupahFox <31490939+SupahFox@users.noreply.github.com>
2020-07-29 20:45:54 -0300

    Update owoify.js

--- comandos/owoify.js
+++ comandos/owoify.js
@@ -5,12 +5,13 @@ const neko = new nekoslife();
 
 module.exports = async(client, message, args) => {
 try{
-let say = message.content.split(' ').slice(1).join(' ')
+ let say = args.join(" ")
  let owoify = await neko.sfw.OwOify({text: say});
  message.delete({ timeout: 100 })
  if(!say) return message.reply("debes ponerme algo para que lo repita owo");
- console.log(owoify)
- message.channel.send(`${owoify.owo}`)
+ if(say.length > 200) return message.reply("Debes ingresar algo menor a 200 caracteres!")
+  console.log(owoify)
+ message.channel.send(`**${message.member.displayName}** dice: ${owoify.owo}`)
 }catch (err) {
     console.log(err);
     return message.reply("Hubo un error"); // estoo
