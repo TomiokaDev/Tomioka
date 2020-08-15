5a470457ac8aa3a11e0dbc697cd0a714a834f367
SupahFox <31490939+SupahFox@users.noreply.github.com>
2020-08-14 21:30:33 -0300

    Tomioka v1.0

--- eventos/ready.js
+++ eventos/ready.js
@@ -5,15 +5,16 @@ module.exports = (client, member) => {
  
   setInterval(function() {
 
-    var estados = [`tk!help | Estoy en ${client.guilds.cache.size} servidores.`, `tk!help | Veo ${client.users.cache.size} usuarios.`, `tk!report por algun fallo o sugerencia.`, `Bot en desarrollo!`, `tk!invite | Estoy en ${client.guilds.cache.size} servidores.`, `tk!invite | Veo ${client.users.cache.size} usuarios.`]
+    var estados = [`tk!help |v1.0| Estoy en ${client.guilds.cache.size} servidores.`, `tk!help |v1.0| Veo ${client.users.cache.size} usuarios.`, `tk!report por algun fallo o sugerencia.`, `Desarrollo pausado hasta nuevo aviso!`, `tk!invite |v1.0| Estoy en ${client.guilds.cache.size} servidores.`, `tk!invite |v1.0| Veo ${client.users.cache.size} usuarios.`]
 
     let estado = estados[Math.floor(estados.length * Math.random())];
 
     client.user.setPresence({
-      status: "online",
+      status: "dnd",
       activity: {
         name: estado,
         type: "WATCHING",
+        url: "https://www.twitch.tv/sunha07"
       }
     })
 
