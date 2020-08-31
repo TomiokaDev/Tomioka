a8e577055108f64dcbc5ac490ff00e810b473205
SupahFox <31490939+SupahFox@users.noreply.github.com>
2020-08-31 15:41:05 -0300

    Create poll.js

---
+++ comandos/poll.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+const config = require('../config.js');
+
+module.exports = async(client, message, args) => {
+try{
+ let poll = message.content.split(/ +/).slice(1).join(/ +/)
+ if(!poll) return message.reply("Debes poner algo!");
+
+const embed = new Discord.MessageEmbed()
+ .setTitle(`Encuesta!`)
+ .setDescription(poll)
+ .addField("Si:", ":white_check_mark:")
+ .addField("No:", ":x:")
+ .setFooter(`Ejecutado por: ${message.author.tag}`, message.author.avatarURL())
+ .setColor(config.color)
+ message.channel.send({embed: embed}).then(embed => {
+    embed.react('✅')
+    embed.react('❌')
+  });
+}catch (err){
+   console.log(err);
+   return message.reply("Hubo un error");
+}
+};
+module.exports.config = {
+command:"poll",
+aliases:["poll"],
+cooldown: 5
+}
