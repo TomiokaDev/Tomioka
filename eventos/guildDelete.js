9d50bb8706b33978087024c373686cf0a71cbde1
SupahFox <31490939+SupahFox@users.noreply.github.com>
2020-09-01 19:29:28 -0300

    Update guildDelete.js

--- eventos/guildDelete.js
+++ eventos/guildDelete.js
@@ -1,6 +1,13 @@
 const Discord = require('discord.js');
 const config = require('../config.js');
 module.exports = async (client, guild) => {
+try{
+const promises = [
+         client.shard.fetchClientValues('guilds.cache.size'),
+      ];
+Promise.all(promises).then(results => {
+         
+         const guilds = results[0].reduce((acc, guildCount) => acc + guildCount, 0);
 
  let md = client.users.cache.get("178651638209314816")
   const embed = new Discord.MessageEmbed()
@@ -13,4 +20,8 @@ module.exports = async (client, guild) => {
   .setFooter(`Noticia del bot ${client.user.username}`)
   .setThumbnail(guild.iconURL({ dynamic: true, format: 'png', size: 1024 }))
   md.send({ embed: embed })
-  }
+  })
+}catch (err) {
+      console.log(err);
+}                  
+}
