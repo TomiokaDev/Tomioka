2d7725ad95ff75df0e22ec620d751c55302a4034
SupahFox <31490939+SupahFox@users.noreply.github.com>
2020-08-31 21:18:44 -0300

    Update ping.js

--- comandos/ping.js
+++ comandos/ping.js
@@ -2,10 +2,12 @@ const Discord = require('discord.js');
 const config = require('../config.js'); 
 
 module.exports = (client, message, args) => {
-    let ping = Math.floor(message.client.ws.ping)
+    var mping = Date.now() - message.createdTimestamp + " ms"
+    let ping = Math.floor(message.client.ws.ping) + " ms"
     const embed = new Discord.MessageEmbed()
-    .setTitle("Ping del bot")
-    .setDescription(`${ping}`)
+    .setTitle("Ping")
+    .addField("Ping de Discord API:", ping)
+    .addField("Ping de mensajes:", mping)
     .setColor(config.color)
     .setFooter(`Ejecutado por: ${message.author.tag}`, message.author.avatarURL())
     message.channel.send({ embed: embed })
