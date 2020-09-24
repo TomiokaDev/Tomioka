93ff1cf8331351eb58ebcbd8cf7bb72299d11cfe
SupahFox <31490939+SupahFox@users.noreply.github.com>
2020-09-24 19:24:39 -0300

    Create leave.js

---
+++ comandos/leave.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js');
+const config = require('../config.js');
+const ytdl = require('ytdl-core');
+
+module.exports = async(client, message, channel, args) => {
+if(!["178651638209314816", "312342505033170948"].includes(message.author.id)) return;
+let vc = message.member.voice.channel;
+let botvc = message.guild.voice.channel;
+
+if(!vc) return message.channel.send("No estas en un canal de voz");
+if (!botvc) return message.channel.send("No estoy en ningún canal de voz!");
+
+const permissions = vc.permissionsFor(message.client.user);
+try {
+if(vc){
+message.member.voice.channel.leave();
+return message.channel.send("Bye!");
+}
+} catch (error) {
+console.log(error);
+return message.channel.send("Hubo un error al meterse al canal. " + error);
+}
+}
+module.exports.config = {
+command:"leave",
+aliases:["leave"],
+cooldown: 5
+}
