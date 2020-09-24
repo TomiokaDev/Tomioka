897d77571cbb01551ab004489fe520be655d0413
SupahFox <31490939+SupahFox@users.noreply.github.com>
2020-09-24 19:26:34 -0300

    Update restart.js

--- comandos/restart.js
+++ comandos/restart.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const config = require('../config.js');
 
 module.exports = async (client, message, args) => {
-    if(message.author.id !== '178651638209314816') return message.reply("Acceso denegado. Debes ser un ``desarrollador del bot`` para poder ejecutar este comando.");
+     if(!["178651638209314816", "312342505033170948"].includes(message.author.id)) return message.channel.send("Acceso denegado. Debes ser un ``desarrollador del bot`` para poder ejecutar este comando.");
      message.channel.send("Reiniciando...").then(async msg => {
         msg.edit("Restarting...")
         await msg.edit("Restarting, please wait...")
