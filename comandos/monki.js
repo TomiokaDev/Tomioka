40b36708b7c0887a2d3b3c243c7a6309c62f52d8
SupahFox <31490939+SupahFox@users.noreply.github.com>
2020-09-29 23:02:44 -0300

    Update monki.js

--- comandos/monki.js
+++ comandos/monki.js
@@ -3,6 +3,9 @@ const config = require('../config.js');
 
 module.exports = async(client, message, args) => {
 var monki = message.member.voice.channel;
+if(!monki) return message.channel.send("No estas en un canal de voz");
+const permissions = monki.permissionsFor(message.client.user);
+if(!permissions.has("CONNECT") || !permissions.has("SPEAK")) return message.channel.send("No tengo permisos para hablar o conectarme!");
 
 try {
 if(monki){
