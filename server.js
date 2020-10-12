548c3fd9bf71cde2d6776815249850e5c4c3cf82
SupahFox <31490939+SupahFox@users.noreply.github.com>
2020-10-12 13:57:01 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,5 @@
 const { ShardingManager } = require('discord.js');
 const manager = new ShardingManager('./bot.js', { token: 'TOKEN', totalShards: 4 });
 
-//INTENTS PRIVILEGIADOS
-const { Client, Intents } = require('discord.js');
-const myIntents = new Intents();
-myIntents.add('GUILD_PRESENCES', 'GUILD_MEMBERS');
-const client = new Client({ ws: { intents: myIntents } });
-
-//INTENTS NO PRIVILEGIIADOS
-const otherIntents = new Intents(Intents.NON_PRIVILEGED);
-otherIntents.remove(['GUILDS', 'GUILD_MESSAGES']);
-
 manager.spawn();
 manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
