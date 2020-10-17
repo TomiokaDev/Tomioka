fae5809f67cd3dc3abea6e9f88bfaeaf8112971e
SupahFox <31490939+SupahFox@users.noreply.github.com>
2020-10-17 14:34:36 -0300

    Update leave.js

--- comandos/leave.js
+++ comandos/leave.js
@@ -4,18 +4,22 @@ const ytdl = require('ytdl-core');
 
 module.exports = async(client, message, channel, args) => {
 if(!["178651638209314816", "312342505033170948"].includes(message.author.id)) return;
-
+try{
+let serverQueue = client.queue.get(message.guild.id);
 let vc = message.member.voice.channel;
 if(!vc) return message.channel.send("No estas en un canal de voz");
 
-try {
+if(serverQueue){
+serverQueue.connection.dispatcher.end();
+}
 if(vc){
+client.queue.delete(message.guild.id);
 message.member.voice.channel.leave();
 return message.channel.send("Bye!");
 }
 } catch (error) {
 console.log(error);
-return message.channel.send("Hubo un error al meterse al canal. " + error);
+return message.channel.send("Hubo un error al salir del canal. " + error);
 }
 }
 module.exports.config = {
