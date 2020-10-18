6327dfb3c3d2c504fa5258ff3324afe6248c2354
SupahFox <31490939+SupahFox@users.noreply.github.com>
2020-10-18 00:18:49 -0300

    Create colgate.js

---
+++ comandos/colgate.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+const config = require('../config.js'); 
+
+module.exports = (client, message, args) => {
+let colgate = ["https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/d2/d22416ac2d89e040641ab36be8cf331006a1bcd5_full.jpg"]
+let random = colgate[Math.floor(colgate.length * Math.random())];
+if(["635285489376690209"].includes(message.guild.id)){
+  let owner = message.guild.owner.user
+ const embed = new Discord.MessageEmbed()
+ .setDescription(`Colgate :moyai:`)
+ .setImage(random)
+ .setFooter(`Ejecutado por: ${message.author.tag}`, message.author.avatarURL())
+ .setColor(config.color)
+ message.channel.send({ embed: embed })
+ }else{
+  return message.channel.send("error")
+ }
+ };
+module.exports.config = {
+command:"colgate",
+aliases:["colgate"],
+cooldown: 5
+};
