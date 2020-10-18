f6e2ba833295caa52a5f56d4913cdbb8e88fcdf3
SupahFox <31490939+SupahFox@users.noreply.github.com>
2020-10-18 00:32:30 -0300

    Create zalgo.js

---
+++ comandos/zalgo.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+const config = require('../config.js');
+
+module.exports = async(client, message, args) => {
+try{
+ let say = args.join(" ")
+ const curse = require('curse-text');
+ message.delete({ timeout: 100 })
+ if(!say) return message.reply("ḍ̷́é̶̞b̶̜̓e̸̹͂ṡ̷̱ ̸̝̿p̸͔͒o̵͓͠n̷̻̈́ȩ̴̔r̴͓̈́m̸̼̌ë̴͎́ ̴̦̄a̵͠ͅl̶̩͋g̶̿͜ò̵̗ ̷̛̖p̵̳̽a̴͓͑r̴̞̄a̸̱̎ ̵͖͋q̸̫̎u̴̹͒e̸͈͒ ̵̢͊l̶̲͆ȏ̴̬ ̵̯̑r̶̙͆ě̶̮p̴̗̀ḯ̵̢t̷͇͝ã̵̮");
+ console.log(curse)
+ message.channel.send(`**${message.member.displayName}** dice: ${curse(say)}`)
+}catch (err) {
+    console.log(err);
+    return message.reply("Hubo un error"); // estoo
+}
+};
+module.exports.config = {
+command:"owoify",
+aliases:["owoify"],
+cooldown: 5
+}
