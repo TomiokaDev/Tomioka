15cd6c9b07606ec4439bf2397b3ab8a43e0d89f8
SupahFox <31490939+SupahFox@users.noreply.github.com>
2020-11-05 18:45:49 -0300

    Delete server.js

--- server.js
+++
@@ -1,5 +0,0 @@
-const { ShardingManager } = require('discord.js');
-const manager = new ShardingManager('./bot.js', { token: 'TOKEN', totalShards: 8 });
-
-manager.spawn();
-manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
