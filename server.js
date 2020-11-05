e25ef40f9912ff301dfe09a7c92d5423d1fcf132
SupahFox <31490939+SupahFox@users.noreply.github.com>
2020-11-05 18:55:54 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const { ShardingManager } = require('discord.js');
-const manager = new ShardingManager('./bot.js', { token: 'NTMxNDA4MDY3OTkzMTQxMjQ4.XDHOVA.VMAmNN4PvgD2XWnXW3gAoUdp9gQ', totalShards: 8 });
+const config = require('./config.js');
+const manager = new ShardingManager('./bot.js', { token: config.discordtoken, totalShards: 8 });
 
 manager.spawn();
 manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
