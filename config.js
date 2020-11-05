6d597dd1756974abd169e8e983205a13b7dda1c7
SupahFox <31490939+SupahFox@users.noreply.github.com>
2020-11-05 18:36:35 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 module.exports = {
   prefix: "tk!",
-  color: "#ff4803"
+  color: "#ff4803",
+  youtubeapi: "AIzaSyBidm1IcvxCwmxkFO4WtKPQTEcmuwVSsQI"
 }
 
 //By: Night#0880, en colaboración con Mcdonalds Inc.
