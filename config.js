d5f73d536c0026de7e1f055a33658a25a312c2cf
SupahFox <31490939+SupahFox@users.noreply.github.com>
2020-11-05 18:55:28 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  prefix: "tk!",
+  color: "#ff4803",
+  youtubeapi: "API",
+  discordtoken: "TOKEN",
+  dbltoken: "DBLTOKEN"
+}
+
+//By: Night#0880, en colaboración con Mcdonalds Inc.
