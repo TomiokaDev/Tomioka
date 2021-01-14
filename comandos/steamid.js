5243ee4954b756bf950cbbc39e9751613f51f46a
SupahFox <31490939+SupahFox@users.noreply.github.com>
2021-01-14 00:24:08 -0300

    Create steamid.js

---
+++ comandos/steamid.js
@@ -0,0 +1,57 @@
+const Discord = require('discord.js');
+const config = require('../config.js');
+const { stripIndents } = require("common-tags");
+const fetch = require("node-fetch");
+const dateFormat = require("dateformat");
+
+module.exports = async(client, message, args) => {
+try {
+if(!args[0]) return message.channel.send("Por favor pon el id de un usuario de Steam!")
+const url = `http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=${config.steamapi}&vanityurl=${args.join(" ")}`;
+
+fetch(url).then(res => res.json()).then(body => {
+ if(body.response.success === 42) return message.channel.send("No fui capaz de encontrar un perfil con ese nombre!");
+
+   const id = body.response.steamid;
+   const summaries = `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${config.steamapi}&steamids=${id}`;
+   const bans = `http://api.steampowered.com/ISteamUser/GetPlayerBans/v1/?key=${config.steamapi}&steamids=${id}`
+   const state = ["Offline", "Online", "Busy", "Away", "Snooze", "Looking to trade", "Looking to play"];
+
+     fetch(summaries).then(res => res.json()).then(body => {
+     if(!body.response) return message.channel.send("No fui capaz de encontrar un perfil con ese nombre!");
+     const { personaname, avatarfull, realname, personastate, loccountrycode, profileurl, timecreated } = body.response.players[0];
+
+     fetch(bans).then(res => res.json()).then(body => {
+     if(!body.players) return message.channel.send("No fui capaz de encontrar un perfil con ese nombre!");
+     const { NumberOfVACBans, NumberOfGameBans } = body.players[0];
+
+
+
+
+     const embed = new Discord.MessageEmbed()
+    .setAuthor(`Steam Web API | ${personaname}`, avatarfull)
+    .setThumbnail(avatarfull)
+    .setDescription(stripIndents`**Nombre real:** ${realname || "No especificado"}
+     **Estado:** ${state[personastate]}
+     **País:** :flag_${loccountrycode ? loccountrycode.toLowerCase() : "white"}:
+     **Fecha de creación:** ${dateFormat(timecreated * 1000, "dd/mm/yyyy (h:MM:ss TT)")}
+     **Bans:** VAC: ${NumberOfVACBans}, Game: ${NumberOfGameBans}
+     **Link:** [link del perfil](${profileurl})`)
+    .setColor(config.color)
+    .setFooter(`Ejecutado por: ${message.author.tag}`, message.author.avatarURL())
+     message.channel.send({ embed: embed })
+
+     })
+  })
+})
+
+} catch (err) {
+  console.log(err);
+  return message.reply("Hubo un error al ejecutar el comando D: \n> **Error:** " + err); // estoo
+}
+};
+module.exports.config = {
+command:"steamuser",
+aliases:["steamuser"],
+cooldown: 5
+}
