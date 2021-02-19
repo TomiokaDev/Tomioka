d8fc14de9ac74f5347020ae00fdedefc838f1011
SupahFox <31490939+SupahFox@users.noreply.github.com>
2021-02-19 01:11:05 -0300

    Update serverinfo.js

--- comandos/serverinfo.js
+++ comandos/serverinfo.js
@@ -2,9 +2,13 @@ const Discord = require('discord.js');
 const config = require('../config.js');
 const moment = require("moment");
 
-module.exports = (client, message, args) => {
+module.exports = (client, message, args, guild) => {
   let createdAt = moment(message.guild.createdAt).format('MMMM Do YYYY, h:mm:ss a');
  let owner = message.guild.owner.user
+
+//var memberCount = guild.members.filter(member => !member.user.bot).size;
+const a = client.guilds.cache.get(message.guild.id).memberCount;
+
  const embed = new Discord.MessageEmbed()
  .setTitle(`Server Info`)
  .setDescription(`Este comando te brindará información acerca del servidor que te encuentres actualmente.`)
@@ -13,7 +17,8 @@ module.exports = (client, message, args) => {
  .addField("Creador:", owner.tag)
  .addField("Región:", message.guild.region)
  .addField("Fecha de creación:", createdAt)
- .addField("Cantidad de usuarios:", message.guild.members.cache.size)
+//.addField("Cantidad de usuarios:", message.guild.members.cache.size)
+ .addField("Cantidad de usuarios:", a)
  .addField("Cantidad de canales:", message.guild.channels.cache.size)
  .setThumbnail(message.guild.iconURL({ dynamic: true, format: 'png', size: 1024 }))
  .setFooter(`Ejecutado por: ${message.author.tag}`, message.author.avatarURL())
@@ -25,3 +30,4 @@ command:"serverinfo",
 aliases:["serverinfo"],
 cooldown: 5
 }
+
