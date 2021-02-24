cdc348447cfac5ec97aaa2e074bc931e3737d78d
SupahFox <31490939+SupahFox@users.noreply.github.com>
2021-02-24 20:41:31 -0300

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,19 @@ module.exports = {
   youtubeapi: "API",
   discordtoken: "TOKEN",
   dbltoken: "DBLTOKEN",
-  steamapi: "STEAMAPITOKEN"
+  steamapi: "STEAMAPITOKEN",
+  auth: "top.gg auth",
+  osu_cache_path: "/tmp/osumaps",
+  pp_path: "",
+  beatmap_api: "https://osu.lea.moe",
+  "credentials": {
+    "discord_client_id": "CLIENT ID",
+    "osu_api_key": "OSU API",
+    "twitch_client_id": "",
+    "pexels_key": "",
+    "last_fm_key": ""
+  }
+
 }
 
 //By: Night#0880, en colaboración con Mcdonalds Inc.
