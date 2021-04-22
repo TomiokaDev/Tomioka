6b000788a79fc9b193b364e3009c9b3ffdced074
Fabri <31490939+SupahFox@users.noreply.github.com>
2021-04-22 18:30:57 -0300

    Add files via upload

---
+++ eventos/ready.js
@@ -0,0 +1,39 @@
+// cuando el cliente se enciende corre esto
+// este evento solo se llama una vez cuando el cliente se enciende
+
+module.exports = {
+	name: 'ready',
+	once: true,
+	execute(client) {
+		console.log(`Listo! soy ${client.user.tag}`);
+		
+		const promises = [
+			client.shard.fetchClientValues('guilds.cache.size'),
+			client.shard.broadcastEval('this.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0)'),
+		 ];
+   
+		 Promise.all(promises).then(results => {
+			
+			const guilds = results[0].reduce((acc, guildCount) => acc + guildCount, 0);
+			const users = results[1].reduce((acc, memberCount) => acc + memberCount, 0);
+   
+			setInterval(function () {
+			     var estados = [`tk!help |v1.4.0| Estoy en ${guilds} servers.`, `tk!report por algun fallo o sugerencia.`, `tk!invite |v1.5.0| Estoy en ${guilds} servers.`, `tk!invite |v1.5.0| Veo ${users} usuarios.`, `Mayoría de los problemas arreglados!`]
+		           //var estados = [`uzk!help |v1.4.0| Estoy en ${guilds} servers.`, `uzk!report por algun fallo o sugerencia.`, `uzk!invite |v1.5.0| Estoy en ${guilds} servers.`, `uzk!invite |v1.5.0| Veo ${users} usuarios.`, `Mayoría de los problemas arreglados!`]
+   
+			   let estado = estados[Math.floor(estados.length * Math.random())];
+   
+			   client.user.setPresence({
+				  status: "online",
+				  activity: {
+					 name: estado,
+					 type: "WATCHING",
+					 url: "https://www.twitch.tv/supahfoxeh"
+				  }
+			   })
+   
+   
+			}, 20000);
+		 })
+	},
+};
---
+++ index.js
@@ -0,0 +1,127 @@
+//DEFINIMOS DISCORD.JS Y LOS GATEWAY INTENTS
+const { Client, Intents } = require('discord.js');
+//const myIntents = new Intents();
+//myIntents.add('GUILD_PRESENCES', 'GUILD_MESSAGES');
+const Discord = require('discord.js');
+//const client = new Discord.Client({ ws: { intents: myIntents } });
+const client = new Discord.Client();
+const ytdl = require('ytdl-core');
+client.queue = new Map();
+const fs = require('fs');
+const { token } = require('./config.json');
+const config = require('./config.json');
+
+
+client.comandos = new Discord.Collection();
+client.cooldowns = new Discord.Collection();
+
+//const DBL = require("dblapi.js");
+//const dbl = new DBL(config.dbltoken, client);
+
+const dbl = require('topgg-autoposter');
+const ap = dbl(config.dbltoken, client);
+
+
+const constant = require('./node_modules/discord.js/src/util/Constants.js')
+constant.DefaultOptions.ws.properties.$browser = `Discord iOS`
+
+
+// INTENTS NO PRIVILEGIADOS
+
+//const otherIntents = new Intents(Intents.NON_PRIVILEGED);
+//otherIntents.remove(['GUILDS', 'GUILD_MESSAGES']);
+
+//const otherIntents2 = new Intents(32509);
+//otherIntents2.remove(1, 512);
+
+
+// El modulo fs se utiliza para leer los archivos y carpetas de un directorio:
+let { readdirSync } = require('fs'); 
+let scooldown = new Map();
+
+
+// Referenciamos nuestro archivo de configuración, ahora en JS: 
+
+//Creamos una colección para Discordjs llamada 'comandos':
+
+for(const file of readdirSync('./comandos/')) { 
+
+	//Esto condicion evitara que los archivos que no son archivos .js no coleccione:
+	if(file.endsWith(".js")) { 
+  
+	//Elimina los últimos tres caracteres nombre del archivo para
+	//deshacerse de la extensión .js y solo quedarnos con el nombre del comando:
+	let fileName = file.substring(0, file.length - 3); 
+  
+	//Define una nueva varible 'fileContents' de la exportación del comando 
+	//dentro de la carpeta comandos:
+	let fileContents = require(`./comandos/${file}`); 
+  
+	//Agrega el nombre del comando a la colección client.commands con un 
+	//valor de sus exportaciones respectivas.
+	client.comandos.set(fileName, fileContents);
+	}
+  }
+  
+  // <-- AQUI EL CONTROLADOR DE EVENTOS: -->
+  const eventFiles = fs.readdirSync('./eventos').filter(file => file.endsWith('.js'));
+  //dentro de nuestro for llamamos a la carpeta eventos creada:
+  // lee la carpeta ./comandos
+  const commandFolders = fs.readdirSync('./comandos');
+  // lee la carpeta ./eventos
+for (const file of eventFiles) {
+	const event = require(`./eventos/${file}`);
+	if (event.once) {
+       client.once(event.name, (...args) => event.execute(...args, client));
+	}
+ else {
+       client.on(event.name, (...args) => event.execute(...args, client));
+	}
+}
+for (const folder of commandFolders) {
+    // !! no se pueden dejar archivos sin subcategoria asignada
+    // lee subcarpetas de el directorio ./comandos
+	const commandFiles = fs.readdirSync(`./comandos/${folder}`).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+        // llama al archivo js en cada subcategoria
+		const command = require(`./comandos/${folder}/${file}`);
+		client.comandos.set(command.name, command);
+	}
+}
+		  
+	// Elimina la memoria caché del archivo requerido para facilitar la recarga y no 
+	// tener más memoria de la necesaria.
+  
+	  //let guildMemberAdd = file.split(".")[0];
+	  
+	//client.on(guildMemberAdd, event.bind(null, client));
+	  //delete require.cache[require.resolve(`./eventos/${file}`)]; 
+
+  
+  
+  // <-- PROPIEDAD LOGIN: -->
+  
+  // Inicia sesión en Discord con el token definido en config.
+  client.login(config.discordtoken) //agregamos las promesas de la propiedad login.
+	.then(() => { 
+	  console.log(`bot started ${client.user.tag}`);
+	  console.log("Node Version: " + process.version);
+	  console.log("Discord.js Version: " + Discord.version);
+  
+	})
+	.catch((err) => {
+  
+	  //Si se produce un error al iniciar sesión, se le indicará en la consola.
+	  console.error("Error al iniciar sesión: " + err);
+  
+	});
+  
+ ap.on('posted', () => {
+	console.log('Server count posted!');
+  })
+  
+  ap.on('error', e => {
+   console.log(`Error de la API de top.gg! ${e}`);
+  })
+// entra a discord con el token de tu app
+client.login(config.discordtoken);
