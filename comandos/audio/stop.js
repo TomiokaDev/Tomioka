4ca73df32df97a345948c523777e39643eb87c1d
Fabri <31490939+SupahFox@users.noreply.github.com>
2021-05-11 13:33:52 -0300

    Create stop.js

---
+++ comandos/audio/stop.js
@@ -0,0 +1,18 @@
+module.exports = {
+	name: 'stop',
+	description: 'Para una canción.',
+	aliases: ['st'],
+	guildOnly: true,
+	cooldown: 5,
+	args: false,
+	usage: '<canción> / <url>',
+	voiceOnly: true,
+	async execute(message, args) {
+		const queue = message.client.distube.getQueue(message);
+		if (!queue) return message.channel.send(`${message.client.emotes.error} | La queue esta vacia!`);
+		message.client.distube.stop(message);
+		message.channel.send(`${message.client.emotes.stop} | Canción parada.`);
+
+
+	},
+};
