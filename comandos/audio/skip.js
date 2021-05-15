2c9d974be1c835d7c9aa05665cf85ac87b3c406c
Fabri <31490939+SupahFox@users.noreply.github.com>
2021-05-15 15:20:50 -0300

    Update skip.js

--- comandos/audio/skip.js
+++ comandos/audio/skip.js
@@ -8,13 +8,14 @@ module.exports = {
 	async execute(message) {
 
 		const queue = message.client.distube.getQueue(message);
-		if (!queue) return message.channel.send(`${message.client.emotes.error} | La queue esta vacia!`);
+		if (!queue) return message.channel.send(`${message.client.emotes.error} | La queue esta vacia!`).then(message => message.delete({timeout: 10000}));
 		try {
 			message.client.distube.skip(message);
-			message.channel.send(`${message.client.emotes.success} | Skipeado a: \`${queue.songs[1].name}\``);
+			message.channel.send(`${message.client.emotes.success} | Skipeado a: \`${queue.songs[1].name}\``).then(message => message.delete({timeout: 10000}));
 		}
 		catch (e) {
 			message.channel.send(`${message.client.emotes.error} | ${e}`);
 		}
 	},
 };
+
