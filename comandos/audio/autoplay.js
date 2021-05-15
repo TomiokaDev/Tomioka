e37a67bfddeba6c8a748b8a7f1a6dee814ae34f8
Fabri <31490939+SupahFox@users.noreply.github.com>
2021-05-14 22:38:50 -0300

    Update autoplay.js

--- comandos/audio/autoplay.js
+++ comandos/audio/autoplay.js
@@ -5,7 +5,6 @@ module.exports = {
 	cooldown: 5,
 	voiceOnly: true,
 	async execute(message) {
-		if(!["178651638209314816", "312342505033170948"].includes(message.author.id)) return;
 		const mode = message.client.distube.toggleAutoplay(message);
 		message.channel.send('Autoplay puesto en modo:`' + (mode ? 'On' : 'Off') + '`');
 	},
