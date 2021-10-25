27a86f859ce75911c6f603b2169e0c45a245263a
BuildTools <unconfigured@null.spigotmc.org>
2021-10-25 16:12:10 -0300

    build 1.01

---
+++ APP events/ready.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
+// cuando el cliente se enciende corre esto
+// este evento solo se llama una vez cuando el cliente se enciende
+
+
+module.exports = {
+	name: 'ready',
+	once: true,
+	execute(client) {
+		console.log(`Listo! soy ${client.user.tag}`);
+
+	},
+};
