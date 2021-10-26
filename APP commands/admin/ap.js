9125f704249d683d461c9da49659ada61ba8fb53
Fabri <fabuz313@gmail.com>
2021-10-26 16:41:14 -0300

    s

---
+++ APP commands/admin/ap.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js');
+const config = require('../../config.json');
+
+module.exports = {
+	name: 'ap',
+	description: 'Enumera a los enfermos mentales de AP',
+	aliases: ['atencionpersonalizada', 'retard'],
+	guildOnly: true,
+	cooldown: 5,
+	async execute(message, args) {
+
+if(!["178651638209314816", "312342505033170948"].includes(message.author.id)) return;
+ let AP1 = await message.client.users.fetch("434888018067980288");
+ let AP2 = await message.client.users.fetch("696481341566615664");
+ let AP3 = await message.client.users.fetch("524231408407805952");
+
+
+ const embed = new Discord.MessageEmbed()
+ .setTitle("Integrantes de AP (Atención personalizada)")
+ .setDescription("Estos son los discapacitados mentales que están en AP")
+ .addField("AP:", "``" + AP1.tag + "`` " +  "``" + AP2.tag + "`` " + "``" + AP3.tag + "`` ")
+ .setFooter(`texto de abajo`, message.author.avatarURL())
+ .setColor(config.color)
+ .setFooter(`texto de abajo`, message.author.avatarURL())
+ message.channel.send({ embed: embed })
+
+},
+};
---
+++ APP commands/reacciones/trespingos.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+const config = require('../../config.json');
+
+module.exports = {
+	name: 'trespingos',
+	description: 'Comando para que Sonic te diga que le chupa tres pingos',
+	aliases: ['3pingos'],
+	guildOnly: true,
+	cooldown: 5,
+	async execute(message, args) {
+
+ const embed = new Discord.MessageEmbed()
+ .setDescription(`A **${message.author.username}** le chupó 3 pingos`)
+ .setImage("https://cdn.discordapp.com/attachments/671170382010515466/728671363170041926/EZn-iNYWkAEforF.jpg")
+ .setFooter(`Ejecutado por: ${message.author.tag}`, message.author.avatarURL())
+ .setColor(config.color)
+ message.channel.send({ embed: embed })
+
+},
+};
