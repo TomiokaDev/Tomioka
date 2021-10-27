f78861ca2115ca348afdb8aa84b781ae0dd5660e
Fabri <fabuz313@gmail.com>
2021-10-27 20:16:57 -0300

    comandos viejos

---
+++ src/ModuleJS/APP outdated/commands/secrets/monki.js
@@ -0,0 +1,40 @@
+const Discord = require('discord.js');
+const config = require('../../config.json');
+
+module.exports = {
+	name: 'monki',
+	description: 'Comando secreto! 2/6',
+	aliases: ['monkee', 'monke', 'mono', 'primate', 'primategracioso', 'funnymonkee'],
+	guildOnly: true,
+	cooldown: 120,
+	async execute(message, args) {
+
+var monki = message.member.voice.channel;
+if(!monki) return message.channel.send("No estas en un canal de voz");
+const permissions = monki.permissionsFor(message.client.user);
+if(!permissions.has("CONNECT") || !permissions.has("SPEAK")) return message.channel.send("No tengo permisos para hablar o conectarme!");
+
+     let audiom = ['./audio/monki.mp3', './audio/monkiflip.mp3', './audio/funnymonkee.m4a']
+    
+     let random = audiom[Math.floor(audiom.length * Math.random())];
+if(monki){
+
+const connection = await message.member.voice.channel.join();
+const dispatcher = connection.play(random);
+
+dispatcher.on('start', () => {
+     const embed = new Discord.MessageEmbed()
+    .setTitle("MONKI")
+    .setImage("https://cdn.discordapp.com/attachments/671170382010515466/760950452124123166/monkiswim.gif")
+    .setColor(config.color)
+    .setFooter(`Comando secreto! 2/6`, message.author.avatarURL())
+    return message.channel.send({ embed : embed });
+});
+
+dispatcher.on('finish', () => {
+        monki.leave();
+});
+
+}
+ },
+};
---
+++ src/ModuleJS/APP outdated/commands/util/idinfo.js
@@ -0,0 +1,47 @@
+const Discord = require('discord.js');
+const moment = require("moment");
+const config = require('../../config.json');
+
+module.exports = {
+	name: 'idinfo',
+	description: 'Comando para mostrar info de ID de usuario',
+	aliases: ['infoid'],
+	guildOnly: true,
+	cooldown: 5,
+	async execute(message, args) {
+
+if(isNaN(args[0])) return message.channel.send("Ingresa una ID")
+if(args[0].length < 12) return message.channel.send("ID invalida")
+let id = await message.client.users.fetch(args[0]);
+if(!id) return message.channel.send("Usuario no encontrado. Ingresa una ID válida")
+
+let flags = {
+   DISCORD_EMPLOYEE: "<:discordemployee:738156738326298625>",
+   DISCORD_PARTNER: "<:partner:738156787076956301>",
+   HYPESQUAD_EVENTS: "<:HypeSquadEvents:738156771675340802>",
+   BUGHUNTER_LEVEL_1: "<:Bughunterlevel1:738156708333092886>",
+   HOUSE_BRAVERY: "<:bravery:738156680134524945>",
+   HOUSE_BRILLIANCE: "<:briliance:738156695531814932>",
+   HOUSE_BALANCE: "<:balance:738156646165118987>",
+   EARLY_SUPPORTER: "<:earlysupport:738156753925046382>",
+   TEAM_USER: "<:TeamUser:738156883424182372>",
+   SYSTEM: "<:system:738156800213385217>",
+   BUGHUNTER_LEVEL_2: "<:Bughunterlevel2:738156724564787301>",
+   VERIFIED_BOT: "<:botverified:738156663978197112>",
+   VERIFIED_DEVELOPER: "<:VerifiedDeveloper:738156897412317255>",
+}
+  let emoji =  id.flags ? id.flags.toArray().map(f => flags[f]).join(" ") : "";
+  const embed = new Discord.MessageEmbed()
+ .addField("Username", id.tag)
+ .addField("ID", id.id)
+ .addField("Fecha de creación", id.createdAt)
+  if(emoji.length > 0)
+  embed.addField("Insignias", emoji);
+  embed
+ .setThumbnail(id.avatarURL({ dynamic: true, format: 'png', size: 1024 }))
+ .setFooter(`Ejecutado por: ${message.author.tag}`, message.author.avatarURL())
+ .setColor(config.color)
+ message.channel.send({ embed: embed })
+
+},
+};
