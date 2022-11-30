a8bb649eaa55106738e65e5b935fe4ebf80192a9
Fabri <31490939+SupahFox@users.noreply.github.com>
2022-11-29 23:06:19 -0300

    modified:   package-lock.json modified:   package.json deleted:    slashCommands/reacciones/chocomilk modified:   slashCommands/util/ascii.js modified:   slashCommands/util/avatar.js deleted:    slashCommands/util/idinfo.js deleted:    slashCommands/util/info.js deleted:    slashCommands/util/invite.js deleted:    slashCommands/util/ping.js deleted:    slashCommands/util/poll.js deleted:    slashCommands/util/say.js deleted:    slashCommands/util/userinfo.js

--- slashCommands/util/ascii.js
+++ slashCommands/util/ascii.js
@@ -1,18 +1,24 @@
 const Discord = require('discord.js');
+const { ApplicationCommandType, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ApplicationCommandOptionType } = require('discord.js');
 
 module.exports = {
 	name: 'ascii',
 	description: 'Convierte el texto a formato ASCII',
-	aliases: ['asciitext', 'textoascii'],
 	guildOnly: true,
 	cooldown: 5,
+  type: ApplicationCommandType.ChatInput,
+  options: [
+    {
+        name: 'texto ascii',
+        description: 'Texto que se convertirá a ascii',
+        type: ApplicationCommandOptionType.String,
+        required: true
+    }
+],
 	run: (client, interaction) => {
+ const figlet = require('figlet');
 
- var figlet = require('figlet');
- let say = message.content.split(' ').slice(1).join(' ');
- if(!say) return interaction.reply("Debes poner algo para que lo convierta en formato ascii");
-
- figlet(say, function(err, data) {
+ figlet(interaction.options.get('steamid').value, function(err, data) {
     if (err) {
         interaction.reply("Hubo un error al ejecutar el comando D: \n> **Error:** " + err);
         console.log(err);
--- slashCommands/util/avatar.js
+++ slashCommands/util/avatar.js
@@ -1,21 +1,38 @@
 const Discord = require('discord.js');
 const config = require('../../config.json');
+const { ApplicationCommandType, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ApplicationCommandOptionType } = require('discord.js');
 
 module.exports = {
 	name: 'avatar',
 	description: 'Tu foto de perfil de Discord',
-	aliases: ['pp', 'dsphoto'],
 	guildOnly: true,
 	cooldown: 5,
+	type: ApplicationCommandType.ChatInput,
+	options: [
+		{
+			name: 'Mencion',
+			description: 'Menciona un usuario para ver su avatar',
+			type: ApplicationCommandOptionType.Mention,
+			required: true
+		},	
+	],
 	run: (client, interaction) => {
-
-const user = message.mentions.users.first() || message.author;
+		console.log(interaction.options.get('Mencion').value);
+if(!interaction.options.get('Mencion').value){
   const embed = new EmbedBuilder()
- .setDescription("Avatar de " + user.tag)
+ .setDescription("Avatar de " +  interaction.member.user.tag)
  .setColor(config.color)
- .setImage(user.avatarURL({ dynamic: true, format: 'png', size: 1024 }))
+ .setImage(interaction.member.user.avatarURL({ dynamic: true, format: 'png', size: 1024 }))
 .setFooter({text: `Ejecutado por: ${interaction.member.user.tag}`, iconURL: interaction.member.user.avatarURL()})
  interaction.reply({ embeds: [embed] })
+}
+
+const embedmencion = new EmbedBuilder()
+.setDescription("Avatar de " +  interaction.options.user.tag)
+.setColor(config.color)
+.setImage(interaction.options.user.avatarURL({ dynamic: true, format: 'png', size: 1024 }))
+.setFooter({text: `Ejecutado por: ${interaction.member.user.tag}`, iconURL: interaction.member.user.avatarURL()})
+interaction.reply({ embeds: [embedmencion] })
 
 },
 };
