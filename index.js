ca366a06380f5bd329e7a6b6f0fdf4598935e5cc
Fabri <SupahFox@github.com>
2022-11-30 17:21:43 -0300

    a

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const { ShardingManager } = require('discord.js');
 const Topgg = require("@top-gg/sdk");
 const express = require("express");
 const app = express();
+const config = require('./config.json');
+require('dotenv').config()
 const manager = new ShardingManager('./bot.js', { token: process.env.TOKEN, totalShards: 'auto' });
 const webhook = new Topgg.Webhook(process.env.DBLTOKEN);
 manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
@@ -13,6 +15,6 @@ app.post("/dblwebhook", webhook.middleware(), (req, res) => {
     console.log(req.vote.user); // 395526710101278721 < user who voted
   });
   
-  app.listen(9068, "ubuntu", () => { // setting port for existing server
+  app.listen(9068, "fabuz", () => { // setting port for existing server
      console.log('Https listening for top.gg'); // Now socket and server both listens to same port
   });
