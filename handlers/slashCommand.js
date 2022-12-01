d90533aeb707c9074197d6e2c392598600a2fd7e
Fabri <31490939+SupahFox@users.noreply.github.com>
2022-11-30 23:40:34 -0300

    modified:   .env

--- handlers/slashCommand.js
+++ handlers/slashCommand.js
@@ -41,12 +41,12 @@ module.exports = (client) => {
 
 	(async () => {
 			try {
-                /*rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), { body: [] })
+                rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), { body: [] })
 				.then(() => console.log('Successfully deleted all guild commands.'))
 				.catch(console.error);
 
 				// for global commands
-				rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: [] })
+				/*rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: [] })
 				.then(() => console.log('Successfully deleted all application commands.'))
 				.catch(console.error);*/
 
