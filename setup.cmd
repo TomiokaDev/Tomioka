a3fcc8d20443ac6dcc5f81a58e34f50e25165d66
SupahFox <31490939+SupahFox@users.noreply.github.com>
2023-07-07 19:27:12 -0300

    setup cmd

--- setup.cmd
+++ setup.cmd
@@ -1,7 +1,7 @@
-@echo off
-npm i
-npm i -g forever
-pip install -r requirements.txt
-echo "Copyright © TomiokaBot Development 2019 - 2023. All rights reserved.
-echo Press enter to exit
+@echo on
+call npm i
+call npm i -g forever
+call pip install -r requirements.txt 
+echo "Copyright © TomiokaBot Development 2019 - 2023. All rights reserved."
+echo "Press enter to exit"
 set /p input=
