f58babf755bcde4bca0589735489ba0d681c0145
SupahFox <31490939+SupahFox@users.noreply.github.com>
2023-07-07 15:48:38 -0300

    python

---
+++ python/openai.py
@@ -0,0 +1,8 @@
+from OpenAIAuth import Auth0
+# Declarar para leer envs
+from dotenv import load_dotenv
+load_dotenv()
+import os
+auth = Auth0(email=os.getenv("OPENAI_EMAIL"), password=os.getenv("OPENAI_PASSWORD"))
+access_token = auth.get_access_token()
+print(access_token)
