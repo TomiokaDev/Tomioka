15fa7696aaeb7713964bb46d95b94b16f55ca80d
Fabri <31490939+SupahFox@users.noreply.github.com>
2023-07-25 18:52:11 -0300

    Update characterai.py

--- python/characterai.py
+++ python/characterai.py
@@ -1,1 +1,19 @@
+# TomiokaBot
+# By: @SupahFox
+# Description: character.ai integration
+
 # TODO: ESTA COSA
+
+from characterai import PyCAI
+
+client = PyCAI('TOKEN')
+
+while True:
+    message = input('You: ')
+    
+    data = client.chat.send_message('CHAR', message)
+    
+    message = data['replies'][0]['text']
+    name = data['src_char']['participant']['name']
+    
+    print(f"{name}: {message}")
