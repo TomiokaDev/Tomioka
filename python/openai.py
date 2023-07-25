0468cfec43d4bffafbfef357ca936ccd9537dd6f
Fabri <31490939+SupahFox@users.noreply.github.com>
2023-07-25 18:42:43 -0300

    Update openai.py

--- python/openai.py
+++ python/openai.py
@@ -8,5 +8,6 @@ from dotenv import load_dotenv
 load_dotenv()
 import os
 auth = Auth0(email=os.getenv("OPENAI_EMAIL"), password=os.getenv("OPENAI_PASSWORD"))
+# NO PIENSO PAGAR 20USD AL MES POR EL GPT -4 ASÍ QUE VOY A USAR EL 3.5 PORQUE SOY POBRE
 access_token = auth.get_access_token()
 print(access_token)
