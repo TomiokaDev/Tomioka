dd848651310d0e8c80f146a4142f9d117517c0ea
SupahFox <31490939+SupahFox@users.noreply.github.com>
2023-10-25 19:54:28 -0300

    remplazo de npm por pnpm

--- setup.cmd
+++ setup.cmd
@@ -1,6 +1,7 @@
 @echo on
-call npm i
-call npm i -g forever
+call npm install -g pnpm
+call pnpm i
+call pnpm i -g forever
 call pip install -r requirements.txt 
 echo "Copyright © TomiokaBot Development 2019 - 2023. All rights reserved."
 echo "Press enter to exit"
